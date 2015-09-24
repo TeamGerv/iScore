cd10609707a8d89b7a7c786ac5d241b29e7833b9
Vuong <ak90260@logti-a3412-07.local>
2015-09-24 17:30:58 -0400

    Initial Commit

---
+++ iScore/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iScore
+//
+//  Created by Vuong, Arron on 2015-09-24.
+//  Copyright (c) 2015 Vuong, Arron. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iScore/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iScore
+//
+//  Created by Vuong, Arron on 2015-09-24.
+//  Copyright (c) 2015 Vuong, Arron. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
