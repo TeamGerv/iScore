1aa93cbb2e0c29fab29ca5f5577e70355ede7194
Vuong <ak90260@logti-a3412-07.local>
2015-09-24 21:36:09 -0400

    ds/

--- iScore/ViewController.h
+++ iScore/ViewController.h
@@ -8,7 +8,12 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController <UITableViewDelegate, UITableViewDataSource>{
+    IBOutlet UITableView *tableView;
+    
+    NSArray *mainArray;
+}
+
 
 
 @end
--- iScore/ViewController.m
+++ iScore/ViewController.m
@@ -7,8 +7,12 @@
 //
 
 #import "ViewController.h"
+#import <UIKit/UIKit.h>
 
 @interface ViewController ()
+- (IBAction)newButton:(UIButton *)sender;
+@property (weak, nonatomic) IBOutlet UILabel *round;
+
 
 @end
 
@@ -17,11 +21,43 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    
+    mainArray = [[NSArray alloc] initWithObjects:@"one",@"two",@"three",@"four",@"five", nil];
+}
+
+- (NSInteger) tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    return [mainArray count];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"thisCell"];
+    cell.textLabel.text = @"yo";//[mainArray objectAtIndex:indexPath.row];
+    return cell;
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+
+
+- (void)alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex{
+    self.round.text =[[alertView textFieldAtIndex:0] text];
+    
+    
+    NSLog(@"Entered: %@",[[alertView textFieldAtIndex:0] text]);
+}
+
+
+- (IBAction)newButton:(UIButton *)sender {
+    
+    UIAlertView * alert = [[UIAlertView alloc] initWithTitle:@"Bienvenue à iScore" message:@"hello bello" delegate:self cancelButtonTitle:@"Continuez" otherButtonTitles:nil];
+    alert.alertViewStyle = UIAlertViewStylePlainTextInput;
+    UITextField * alertTextField = [alert textFieldAtIndex:0];
+    alertTextField.placeholder =@"Enter something..";
+    [alert show];
 }
 
 @end
